884d91549986cac7d79aeb793989468abdfa1c6c
Nayak <jyoti-prakash.nayak@capgemini.com>
2024-10-04 13:00:57 +0530

    code modified

--- Desktop/ticket-management/src/App.js
+++ Desktop/ticket-management/src/App.js
@@ -5,6 +5,7 @@ import ConnectMember from './components/ConnectMember';
 import NestedJsonData from './components/NestedJsonData';
 import UserRegistration from './components/User-Registration/userRegistration';
 import Stopwatch from './components/Stop-Watch/StopWatch';
+import Data from './components/Data';
 
 function App() {
    const [activeComponent, setActiveComponent] = useState(null);
@@ -14,36 +15,38 @@ function App() {
   };
 
   return (
-   <div className="container mt-5">
-   <h1 className="text-center mb-4">Ticket Management System (TMG)</h1>
-   
-   <div className="d-flex justify-content-center mb-4">
-     <button className="btn btn-primary mx-2" onClick={() => handleButtonClick('TicketManagement')}>
-       Ticket Management
-     </button>
-     <button className="btn btn-primary mx-2" onClick={() => handleButtonClick('NestedJsonData')}>
-       Nested Data
-     </button>
-     <button className="btn btn-primary mx-2" onClick={() => handleButtonClick('UserRegistration')}>
-       User Registration
-     </button>
-     <button className="btn btn-primary mx-2" onClick={() => handleButtonClick('StopWatch')}>
-       Stop Watch
-     </button>
-   </div>
+   <><div className="container mt-5">
+      <h1 className="text-center mb-4">Ticket Management System (TMG)</h1>
 
-   {activeComponent === 'TicketManagement' && (
-     <div>
-       <RaiseTicket />
-       <TicketStatus />
-       <ConnectMember />
-     </div>
-   )}
+      <div className="d-flex justify-content-center mb-4">
+        <button className="btn btn-primary mx-2" onClick={() => handleButtonClick('TicketManagement')}>
+          Ticket Management
+        </button>
+        <button className="btn btn-primary mx-2" onClick={() => handleButtonClick('NestedJsonData')}>
+          Nested Data
+        </button>
+        <button className="btn btn-primary mx-2" onClick={() => handleButtonClick('UserRegistration')}>
+          User Registration
+        </button>
+        <button className="btn btn-primary mx-2" onClick={() => handleButtonClick('StopWatch')}>
+          Stop Watch
+        </button>
+      </div>
 
-   {activeComponent === 'NestedJsonData' && <NestedJsonData />}
-   {activeComponent === 'UserRegistration' && <UserRegistration />}
-   {activeComponent === 'StopWatch' && <Stopwatch />} 
- </div>
+      {activeComponent === 'TicketManagement' && (
+        <div>
+          <RaiseTicket />
+          <TicketStatus />
+          <ConnectMember />
+        </div>
+      )}
+
+      {activeComponent === 'NestedJsonData' && <NestedJsonData />}
+      {activeComponent === 'UserRegistration' && <UserRegistration />}
+      {activeComponent === 'StopWatch' && <Stopwatch />}
+    </div>
+    <Data />
+    </>
   );
 };
 
